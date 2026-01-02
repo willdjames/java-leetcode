8510182041fee7d215809da02c462faa00827745
William Santana <williamdiegocampos@gmail.com>
2026-01-01 22:17:31 -0300

    Solucao do Power of four

--- src/PowerOfFourMain.java
+++ src/PowerOfFourMain.java
@@ -1,15 +1,26 @@
 // https://leetcode.com/problems/power-of-four/
 
 boolean isPowerOfFour(int n) {
-
-
-
-
-    return false;
+    if(n == 0){
+        return false;
+    }
+    while (n % 4 == 0) {
+        n = n / 4;
+    }
+    return n == 1;
 }
 
 
 void main() {
-    int input = -16;
-    IO.println(isPowerOfFour(input));
+    int caseA = -16;
+    int caseB = 16;
+    int caseC = 5;
+    int caseD = 1;
+    int caseE = 0;
+
+    assert !isPowerOfFour(caseA) : "caseA";
+    assert isPowerOfFour(caseB) : "caseB";
+    assert !isPowerOfFour(caseC) : "caseC";
+    assert isPowerOfFour(caseD) : "caseD";
+    assert !isPowerOfFour(caseE) : "caseE";
 }
